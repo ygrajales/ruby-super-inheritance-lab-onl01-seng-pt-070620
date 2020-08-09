b8c3c07f7f6e767aba1f43ebbb15996d4f7d9a92
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-08-09 21:06:49 +0000

    Automatically backed up by Learn

---
+++ lib/chatty_student.rb
@@ -0,0 +1,15 @@
+require_relative "./student.rb"
+
+class Chatty_Student < student
+  
+  def hello
+    super
+      puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers?  Okay well let me just tell you who died..."
+    end
+    
+    
+  
+  
+  
+  
+end
